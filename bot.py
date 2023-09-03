e2716ebcec76d16752a89e713a13875f4b856705
palm-octo-chainsaw <kristiyan.georgiev.bs@gmail.com>
2023-09-03 19:01:20 +0300

    asd

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from instabot import Bot
 
 try:
     bot = Bot()
-    bot.login(username=sys.argv[1], password=sys.argv[2], proxy=f"{sys.argv[1]}:{sys.argv[2]}@10.10.1.10:3128")
+    bot.login(username=sys.argv[1], password=sys.argv[2]) # , proxy=f"{sys.argv[1]}:{sys.argv[2]}@10.10.1.10:3128"
     bot.send_message('Fat fuck!', ["nikola.diankov"])
     logging.info('Message sent!')
 except:
