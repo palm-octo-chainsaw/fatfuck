3cd06d8bdd1fef42e591efa96000478ad0507a0a
palm-octo-chainsaw <kristiyan.georgiev.bs@gmail.com>
2023-09-03 18:28:03 +0300

    asd

--- bot.py
+++ bot.py
@@ -1,8 +1,14 @@
 import sys
+import logging
 from instabot import Bot
 
-
-bot = Bot()
-bot.login(username=sys.argv[1], password=sys.argv[2])
-bot.send_message('Fat fuck!', ["nikola.diankov"])
-bot.logout()
+try:
+    bot = Bot()
+    bot.login(username=sys.argv[1], password=sys.argv[2])
+    bot.send_message('Fat fuck!', ["nikola.diankov"])
+    logging.info('Message sent!')
+except:
+    logging.error('Error!')
+finally:
+    bot.logout()
+    logging.info('Logged out!')
